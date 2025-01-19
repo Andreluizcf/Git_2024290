4a5d8cd4ad205696f377b2c9543b28cca249f250
Andreluizcf <andreluizcf.2001@gmail.com>
2025-01-19 06:57:16 +0000

    Second commit
    A new line of code added which will read a word and make some changes in this word.

--- JavaApplication18/src/javaapplication18/JavaApplication18.java
+++ JavaApplication18/src/javaapplication18/JavaApplication18.java
@@ -4,6 +4,8 @@
  */
 package javaapplication18;
 
+import java.util.Scanner;
+
 /**
  *
  * @author dange
@@ -15,6 +17,40 @@ public class JavaApplication18 {
      */
     public static void main(String[] args) {
         System.out.println("Andre - 2024290");
+        
+                Scanner scan = new Scanner(System.in);
+        String name;
+
+        System.out.print("Enter your name (letters only): "); // Ask for an input
+
+        while (true) {
+            name = scan.nextLine();
+
+            if (name.matches("[a-zA-Z]+")) { // Check if the input contains only letters
+                break; // Valid input, exit the loop
+            } else { // Error message, the input didn't contain only letters
+                System.out.println("The name should only contain letters. Please, try again: ");
+            }
+        }
+
+        name = name.trim(); // Exclude any "blank space" that might have before and after the input
+
+        String upperCaseName = name.toUpperCase(); // Convert name to uppercase letter
+
+        String lowerCaseName = name.toLowerCase(); // Convert name to lowercase letter
+
+        if (name.matches("[a-z]+")) { // The name have only lowercase, so I will output only the uppercase change
+
+            System.out.println("That is your name changed to uppercase letters: " + upperCaseName);
+        } else if (name.matches("[A-Z]+")) { // The name have only uppercase, so I will output only the lowercase change
+
+            System.out.println("That is your name changed to lowercase letters: " + lowerCaseName);
+        } else {  // The name is mixed with lowercase and uppercase, so I will output both chnages
+            System.out.println("That is your name changed to uppercase letters: " + upperCaseName);
+            System.out.println("That is your name changed to lowercase letters: " + lowerCaseName);
+        }
+
+        
         
     }
     
